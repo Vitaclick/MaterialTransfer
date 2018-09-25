eda7b7cd1b22aaf6ac010c7b3c04e7318920d761
Vitaclick <vitaliy.zema@gmail.com>
2018-09-25 12:17:43 +0300

    connect to sheet

--- Command.cs
+++ Command.cs
@@ -14,10 +14,7 @@ namespace MaterialTransfer
   [Transaction(TransactionMode.Manual)]
   public class Command : IExternalCommand
   {
-    public Result Execute(
-      ExternalCommandData commandData,
-      ref string message,
-      ElementSet elements)
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
     {
       UIApplication uiapp = commandData.Application;
       UIDocument uidoc = uiapp.ActiveUIDocument;
@@ -27,6 +24,22 @@ namespace MaterialTransfer
       var db = new DBConnector();
       var materialsFromDB = db.ReadSheetData("Общий!A:C");
 
+      try
+      {
+        foreach (var dbMaterial in materialsFromDB)
+        {
+          var dbMatName = dbMaterial[0] as string;
+          // Create new material to doc
+          var newMaterial = Material.Create(doc, dbMatName);
+          var material = doc.GetElement(newMaterial) as Material;
+
+        }
+      }
+      
+      catch (Exception ex)
+      {
+        return Result.Failed;
+      }
       return Result.Succeeded;
     }
   }
